494ae64bf50ecf425f9601a3c0d3ac7b720699fc
Nicolas Limage <nlimage@scaleway.com>
2022-06-09 23:05:33 +0200

    add briq count, rework args

--- cmdgroupgive.go
+++ cmdgroupgive.go
@@ -10,38 +10,43 @@ import (
 )
 
 func init() {
-	MyRoundCmd.Flags().String("group", "", "group name")
-	MyRoundCmd.Flags().String("message", "🎁 #Community", "message to send")
-	MyRoundCmd.MarkFlagRequired("group")
-	Register(&MyRoundCmd)
+	GroupGiveCmd.Flags().String("to-group", "", "group name")
+	GroupGiveCmd.Flags().String("message", "🎁 #Community", "message to send")
+	GroupGiveCmd.Flags().Uint("amount", 1, "how many briqs to give")
+	GroupGiveCmd.MarkFlagRequired("group")
+	Register(&GroupGiveCmd)
 }
 
-var MyRoundCmd = cobra.Command{
+var GroupGiveCmd = cobra.Command{
 	Use:     "group-give",
 	Aliases: []string{"gg"},
 	Short:   "Give one or more briqs to a group",
 	Args:    cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		groupName, err := cmd.Flags().GetString("group")
+		argToGroup, err := cmd.Flags().GetString("to-group")
 		cobra.CheckErr(err)
-		message, err := cmd.Flags().GetString("message")
+		argMessage, err := cmd.Flags().GetString("message")
 		cobra.CheckErr(err)
-		groupUserNames := viper.GetStringSlice(fmt.Sprintf("groups.%s", groupName))
+		argAmount, err := cmd.Flags().GetUint("amount")
+		cobra.CheckErr(err)
+		groupUserNames := viper.GetStringSlice(fmt.Sprintf("groups.%s", argToGroup))
 		client, err := briq.NewClient(viper.GetString(viperKeyBriqSecretKey))
 		cobra.CheckErr(err)
 		req := &briq.ListUsersRequest{}
 		res, err := client.ListUsers(cmd.Context(), req)
 		cobra.CheckErr(err)
 		for _, user := range utils.FilterSlice(res.Users, groupUserNames, BriqUserKey) {
-			fmt.Printf("Sending gift to %s (%v)\n", user.Username, user.Id)
-			req := &briq.CreateTransactionRequest{
-				App:     briq.AppGive,
-				To:      user.Id,
-				Comment: message,
+			for i := uint(0); i < CapBriqAmount(argAmount); i++ {
+				fmt.Printf("Sending gift to %s (%v)\n", user.Username, user.Id)
+				req := &briq.CreateTransactionRequest{
+					App:     briq.AppGive,
+					To:      user.Id,
+					Comment: argMessage,
+				}
+				res, err := client.CreateTransaction(cmd.Context(), req)
+				cobra.CheckErr(err)
+				cobra.CheckErr(Render(res))
 			}
-			res, err := client.CreateTransaction(cmd.Context(), req)
-			cobra.CheckErr(err)
-			cobra.CheckErr(Render(res))
 		}
 	},
 }
